6c2cdb6c3b4dddc207799fb06245f710d42a3262
liushan-nas <liushan@example.com>
2025-11-19 21:19:33 +0800

    Add default RELAY_SERVERS constant set to liushan.vip

--- src/config.rs
+++ src/config.rs
@@ -107,6 +107,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["liushan.vip"];
+pub const RELAY_SERVERS: &[&str] = &["liushan.vip"];
 pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
